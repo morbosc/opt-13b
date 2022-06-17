6b32d6465e490c131583f25433209ad354ab0bde
Azatoth <morbosc@users.noreply.github.com>
2022-06-17 13:50:00 -0400

    Fix path to config/model

--- chat.py
+++ chat.py
@@ -2,8 +2,8 @@ from transformers import GPT2Tokenizer, OPTForCausalLM
 import os
 #this doesn't work, because of course the primary software for a multi-billion dollar industry is stupid
 os.environ["CUDA_VISIBLE_DEVICES"]="-1"
-model = OPTForCausalLM.from_pretrained("opt/13b/metaseq/projects/OPT/models/13b")
-tokenizer = GPT2Tokenizer.from_pretrained("opt/13b/metaseq/projects/OPT/models/13b")
+model = OPTForCausalLM.from_pretrained("/opt/13b/metaseq/projects/OPT/models/13b")
+tokenizer = GPT2Tokenizer.from_pretrained("/opt/13b/metaseq/projects/OPT/models/13b")
 system('clear')
 prompt = "AI: I am an artificial intelligence.\nHuman: What do you do?\nAI: I will respond to your questions, answering them to the best of my ability.\nHuman: Excellent, what is the capitol of France?\nAI: The capitol of France is Paris."
 print(prompt)
